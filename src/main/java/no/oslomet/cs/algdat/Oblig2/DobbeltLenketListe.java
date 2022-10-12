89351c1c914b7c3ba324322f29109129ec2bc489
githubniklas <91746018+githubniklas@users.noreply.github.com>
2022-10-12 15:28:06 +0200

    Implementert metoden iterator<T> iterator()

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -345,7 +345,7 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     @Override
     public Iterator<T> iterator() {
-        throw new UnsupportedOperationException();
+        return new DobbeltLenketListeIterator();
     }
 
     public Iterator<T> iterator(int indeks) {
